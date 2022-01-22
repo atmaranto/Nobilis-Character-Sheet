f966c380e682e4937266a447ee8969b3500f9005
atmaranto <35581227+atmaranto@users.noreply.github.com>
2022-01-22 01:24:38 -0600

    Updated copyright date

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2020 Anthony Maranto
+Copyright (c) 2022 Anthony Maranto
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
